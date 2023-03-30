fab58b050cd95c2ac42ddcd398fc6f1dae14a429
valeria molivo <valeriamolivo@gmail.com>
2023-03-29 23:56:32 -0300

    primer carga

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => { 
+    return (
+        <nav>
+            <h3>Tienda</h3>
+            <div>
+                <button>Juguetes</button>
+                <button>Alimento</button>
+                <button>Ropa</button>
+                <button>Ambientación</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
